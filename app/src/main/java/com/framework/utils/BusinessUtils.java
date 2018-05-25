5fbb9f18e378c5a8a3089ff5deb67e6769265b5b
cuichenxi <784759481@qq.com>
2018-05-25 09:50:55 +0800

    试试

--- app/src/main/java/com/framework/utils/BusinessUtils.java
+++ app/src/main/java/com/framework/utils/BusinessUtils.java
@@ -116,7 +116,7 @@ public class BusinessUtils {
 
     // 检查手机号正确性
     public static boolean checkPhoneNumber(String number) {
-        Pattern p = Pattern.compile("^[1][3,4,5,7,8][0-9]{9}$"); // 验证手机号  ;
+        Pattern p = Pattern.compile("^[1][3,4,5,7,8,9][0-9]{9}$"); // 验证手机号  ;
         Matcher m = p.matcher(number);
         return m.matches();
     }
